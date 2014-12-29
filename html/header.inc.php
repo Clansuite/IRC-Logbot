b9caf2008961a30a3545ed682cd90e4cc35e7118
Jens-André Koch <jakoch@web.de>
2014-12-29 16:39:01 +0100

    doctype update

--- html/header.inc.php
+++ html/header.inc.php
@@ -37,8 +37,8 @@ if(defined('IN_CSLOGBOT') === false)
 }
 include __DIR__ . '/config.inc.php';
 ?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<html>
+<!DOCTYPE html>
+<html lang="de" dir="ltr">
     <head>
         <meta charset="utf-8">
         <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
